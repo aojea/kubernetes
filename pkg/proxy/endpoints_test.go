d0bd44aa37f862551184259249aba21bb0b13831
Andrew Sy Kim <kim.andrewsy@gmail.com>
2020-11-09 10:24:43 -0500

    kube-proxy: update internal endpoints map with
    'serving' and 'terminating' condition from EndpointSlice
    
    Signed-off-by: Andrew Sy Kim <kim.andrewsy@gmail.com>

--- pkg/proxy/endpoints_test.go
+++ pkg/proxy/endpoints_test.go
@@ -23,7 +23,7 @@ import (
 
 	"github.com/davecgh/go-spew/spew"
 
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	discovery "k8s.io/api/discovery/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
--- pkg/proxy/endpointslicecache_test.go
+++ pkg/proxy/endpointslicecache_test.go
@@ -21,7 +21,7 @@ import (
 	"reflect"
 	"testing"
 
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	discovery "k8s.io/api/discovery/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
