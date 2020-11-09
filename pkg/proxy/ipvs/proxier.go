43d46e83043a922759a32c706adda829f3e162d0
Andrew Sy Kim <kim.andrewsy@gmail.com>
2020-11-09 10:24:43 -0500

    TEMP kube-proxy: add ipvs unit tests for handling ready terminating endpoints
    Signed-off-by: Andrew Sy Kim <kim.andrewsy@gmail.com>

--- pkg/proxy/ipvs/proxier.go
+++ pkg/proxy/ipvs/proxier.go
@@ -2087,9 +2087,7 @@ func (proxier *Proxier) syncEndpoint(svcPortName proxy.ServicePortName, onlyNode
 
 			if epInfo.IsReady() {
 				localReadyEndpoints.Insert(epInfo.String())
-			}
-
-			if epInfo.IsServing() && epInfo.IsTerminating() {
+			} else if epInfo.IsServing() && epInfo.IsTerminating() {
 				localReadyTerminatingEndpoints.Insert(epInfo.String())
 			}
 		}
