f430e8a7659cd6031cacff6bc20acf954a00ec1c
Eytan Heidingsfeld <eytanh@google.com>
2021-08-12 14:15:42 +0300

    install-extra-node-requirements: Add installation of apparmor profile for M4A on all GKE nodes
    Change-Id: I4bd10c47e3fdcd7800975be8267728a485cf1e81
    Change-Id: Ic59cf2197088cb378d5c673072971478ea366d10
    Change-Id: I4bbf44069eb983070c5b1e47ef16fa9f5ab8032c

--- gke/cluster/gce/gci/gke-internal-configure.sh
+++ gke/cluster/gce/gci/gke-internal-configure.sh
@@ -3,6 +3,8 @@ NPD_CUSTOM_PLUGINS_VERSION="${NPD_CUSTOM_PLUGINS_VERSION:-v1.0.4}"
 NPD_CUSTOM_PLUGINS_TAR_HASH="${NPD_CUSTOM_PLUGINS_TAR_HASH:-b048ce6daf072a600d9d34997b1e23f8190976f902cf91e6e479aba89202c3ddc5116e2511ce95e842942ca93654f29fa377f1ad93d294f6d07c202d5352c9df}"
 NPD_CUSTOM_PLUGINS_RELEASE_PATH="${NPD_CUSTOM_PLUGINS_RELEASE_PATH:-https://storage.googleapis.com/gke-release}"
 
+M4A_APPARMOR_PROFILE_HASH="${M4A_APPARMOR_PROFILE_HASH:-cd84b52e756bee90b4a26612b372519ebf942bb3b6145d1928d3c1ae0faa4a17ea040f3e5f0429df9193dfcf84364d6a4ac56ebefb70420ae12579be5c5b5756}"
+M4A_APPARMOR_RELEASE_PATH="${M4A_APPARMOR_RELEASE_PATH:-https://storage.googleapis.com/anthos-migrate-release}"
 # Install node problem detector custom plugins.
 function install-npd-custom-plugins {
   local -r version="${NPD_CUSTOM_PLUGINS_VERSION}"
@@ -15,4 +17,42 @@ function install-npd-custom-plugins {
   local -r dir="${KUBE_HOME}/npd-custom-plugins"
   mkdir -p "${dir}"
   tar xzf "${KUBE_HOME}/${tar}" -C "${dir}" --overwrite
+}
+
+function record-preload-info {
+  echo "$1,$2" >> "${KUBE_HOME}/preload_info"
+}
+
+function is-preloaded {
+  local -r key=$1
+  local -r value=$2
+  grep -qs "${key},${value}" "${KUBE_HOME}/preload_info"
+}
+
+function install-m4a-apparmor-profile {
+  local -r hash="${M4A_APPARMOR_PROFILE_HASH}"
+  local -r release_path="${M4A_APPARMOR_RELEASE_PATH}"
+  local -r profile="m4a-apparmor-profile"
+
+  if is-preloaded "${profile}" "${hash}"; then
+    echo "m4a apparmor profile is preloaded."
+    return
+  fi
+
+  if type apparmor_parser; then
+    echo "Downloading ${profile}."
+    if ! [ download-or-bust "${hash}" "${release_path}/artifacts/${profile}" ]; then
+      echo "Failed to download ${profile}, cannot install M4A apparmor profile"
+      return
+    fi
+
+    # This call is expected to fail as the profile is likely not installed.
+    # we are only using this as a safety measure against changes in Ubuntu
+    # image and  installing a clean one
+    sudo apparmor_parser --remove ${KUBE_HOME}/${profile} > /dev/null 2>&1 || true
+    sudo apparmor_parser ${KUBE_HOME}/${profile}
+    record-preload-info "${profile}" "${hash}"
+  else
+    echo "No apparmor_parser found, cannot install M4A apparmor profile"
+  fi
 }
